d821a0034ba3ee73977e1dd2c7bd766d92000ecc
AzureAD\KavinfrancoDevadhas <kdevadh@us.ibm.com>
2017-09-12 10:36:58 -0400

    making DataImportGenerateRandom values

--- ExpenseApp/ExpensesApp/DataImportHelper.cs
+++ ExpenseApp/ExpensesApp/DataImportHelper.cs
@@ -1,33 +1,48 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace ExpensesApp
 {
     class DataImportHelper
     {
+        private static readonly string[] ProbablePeopleNames = { "Ajay", "Bapin", "Cathy", "Darwin", "Elango", "Frank", "Harry" };
+
         public static IList<ExpenseTransactions> ImportListOfTransactions()
         {
-            var expenseTransactions1 = new ExpenseTransactions
-            {
-                Amount = 100,
-                Name = "A",
-                PeopleInTransaction = new List<string> { "A", "B", "C", "D" }
-            };
+            var listOfImportedTransactions = new List<ExpenseTransactions> ();
 
-            var expenseTransactions2 = new ExpenseTransactions
+            for (var i = 0; i < 100; i++)
             {
-                Amount = 500,
-                Name = "B",
-                PeopleInTransaction = new List<string> { "C", "D" }
-            };
-            var expenseTransactions3 = new ExpenseTransactions
+                var randomAmount = new Random();
+                var randomUserPayingBill = SelectRandomPerson();
+                listOfImportedTransactions.Add(item: new ExpenseTransactions{Amount = randomAmount.Next(1,10000),Name = randomUserPayingBill ,PeopleInTransaction = SelectRandomPeopleToBeInTransaction(randomUserPayingBill) });
+            }
+            return listOfImportedTransactions;
+
+        }
+
+        private static IList<string> SelectRandomPeopleToBeInTransaction(string randomUserPayingBill)
+        {
+            var result = new List<string>();
+            var randomPersonInTransactionIterator = new Random();
+            var next = randomPersonInTransactionIterator.Next(1, ProbablePeopleNames.Length - 1);
+
+            while (result.Count < next)
             {
-                Amount = 300,
-                Name = "D",
-                PeopleInTransaction = new List<string> { "A", "B" }
-            };
+                for (var i = 0; i < next; i++)
+                {
+                    var selectRandomPerson = SelectRandomPerson();
+                    if (!result.Contains(selectRandomPerson) && selectRandomPerson!= randomUserPayingBill)
+                        result.Add(selectRandomPerson);
+                }
+            }
+            return result;
+        }
 
-            return new List<ExpenseTransactions> {expenseTransactions1, expenseTransactions2, expenseTransactions3 };
-
+        private static string SelectRandomPerson()
+        {
+            var randomPersonIterator = new Random();
+            return ProbablePeopleNames[randomPersonIterator.Next(0, ProbablePeopleNames.Length-1)];
         }
     }
 }
--- ExpenseApp/ExpensesApp/Program.cs
+++ ExpenseApp/ExpensesApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace ExpensesApp
 {
@@ -8,13 +9,17 @@ namespace ExpensesApp
         static void Main(string[] args)
         {
             Console.WriteLine("Expenses Share App \n");
+            Console.WriteLine("#***************************************#  GENERATING INPUT  #****************************************#\n");
+
             var listOfTransactions = DataImportHelper.ImportListOfTransactions();
             var dictionaryOfPersons=new Dictionary<string,Person>();
 
+            var stopWatch=new Stopwatch();
+            stopWatch.Start();
             foreach (var transaction in listOfTransactions)
             {
                 ExpenseShareAppHelper.AddNewPersonToGroupFromTransaction(dictionaryOfPersons,transaction);
-                ExpenseShareAppHelper.PrintTransactionDetailsForDebugPurpose(dictionaryOfPersons, transaction);
+                //ExpenseShareAppHelper.PrintTransactionDetailsForDebugPurpose(dictionaryOfPersons, transaction);
                 ExpenseShareAppHelper.UpdateLedgerOfPersonsInvolvedInTransaction(dictionaryOfPersons, transaction);
             }
 
@@ -25,6 +30,8 @@ namespace ExpensesApp
                 Console.WriteLine(item.Value.PrintTotalAmountDueOrOwed());
                 Console.WriteLine(item.Value.PrintAmountOwedOrDueByPerPerson());
             }
+            stopWatch.Stop();
+            Console.WriteLine($"The computation of each persons share took {stopWatch.Elapsed.TotalSeconds}");
         }
     }
 }
